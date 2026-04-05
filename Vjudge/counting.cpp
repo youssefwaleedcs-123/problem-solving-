d3777147fd902c4ab61f554c8b9d65275314be00
youssefwaleedcs-123 <youssef.waleed.cs@gmail.com>
2026-04-05 13:52:15 +0200

    Create counting.cpp

---
+++ Vjudge/counting.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int A, B;
+	cin >> A >> B;
+	int counter = 0;
+	for (int i = A; i <= B; i++)
+	{
+		counter++;
+
+	}
+	cout << counter;
+}
