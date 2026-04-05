36c9966b254fb764eadd93fd69a8210e5dd70f19
youssefwaleedcs-123 <youssef.waleed.cs@gmail.com>
2026-04-05 19:18:46 +0200

    Create shift-only.cpp

---
+++ Vjudge/shift-only.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    int N;
+    cin >> N;
+int min_operations = 100;
+ for (int i = 0; i < N; i++)
+    {
+        int num;
+        cin >> num;
+ int current_count = 0;
+        while (num % 2 == 0) {  num = num / 2; current_count = current_count + 1; }
+ if (current_count < min_operations) { min_operations = current_count; }
+    }
+cout << min_operations << endl;
+
