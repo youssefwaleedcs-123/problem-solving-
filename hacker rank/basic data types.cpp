5ff92541f7a872299b1232f4f7f8638fdc4e8f34
youssefwaleedcs-123 <youssef.waleed.cs@gmail.com>
2026-04-05 13:38:36 +0200

    Create basic data types.cpp

---
+++ hacker rank/basic data types.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+#include <iomanip>
+int main()
+{
+ int x;
+ long long y;
+ char h ;
+ float z;
+ double r ;
+ cin>>x>>y>>h>>z>>r;
+ cout<<x<<"\n"<<y<<"\n"<<h<<"\n"<<fixed<<setprecision(3)<<z<<"\n"<<fixed<<setprecision(9)<<r;
+
+ 
+ return 0;
+   
+}   
+
